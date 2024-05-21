fcde4a2264a9dc036ae25bcf967512366ba6cc22
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-21 14:51:38 +0700

    nama dan nilai

--- initialization.cpp
+++ initialization.cpp
@@ -4,5 +4,11 @@ using namespace
 
 class mahasiswa {
 private:
-    const int id:
+    const int id;
+    string nama;
+    float nilai;
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){
+        
+    }
 };

5dd763cd53280c69611b7a25adb00d6da604e3af
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-21 16:17:54 +0700

    comit

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -12,19 +12,5 @@ int main() {
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("dewi");
 
-    varIbu->tambahAnak(varAnak1);
-    varIbu->tambahAnak(varAnak2);
-    varIbu2->tambahAnak(varAnak3);
-    varIbu2->tambahAnak(varAnak1);
 
-    varIbu->cetakAnak();
-    varIbu->cetakAnak();
-
-    delete varIbu;
-    delete varibu2;
-    delete varAnak1;
-    delete varAnak2;
-    delete varAnak3;
-
-    return 0;
 }
